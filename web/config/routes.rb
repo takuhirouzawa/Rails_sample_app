7a9cdad8d8b636e4f0a8a2c4b7f1eb721815a104
takuhiro uzawa <takuhirouzawa@takuhironoMacBook-Pro.local>
2021-08-28 23:23:21 +0900

    Finish user signup

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  resources :users
 end
